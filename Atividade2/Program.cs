2c3fea0a032dd02aeec2bd80f934c6145d4e6358
Moises Pereira Bitencorte <moises.bitencorte@hbsis.com.br>
2020-05-13 16:23:54 -0300

    first commit

---
+++ Atividade2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+
+namespace Atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Larissa");
+            list.Add("Bob");
+            list.Add("Alex");
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list)
+                Console.WriteLine(obj);
+            
+            
+        }
+    }
+}
+
